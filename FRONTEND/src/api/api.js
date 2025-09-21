b4b23965bffe2af7fc9a3aca1c443444fa4eb6bc
jagdeeshkushwah4960 <jagdeeshkushwah4960@gmail.com>
2025-09-21 14:45:54 +0530

    fixed api

--- FRONTEND/src/api/api.js
+++ FRONTEND/src/api/api.js
@@ -1,7 +1,6 @@
-
 import axios from 'axios';
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL|| 'https://job-portal-vc0m.onrender.com'|| process.env.BACKEND_URL ,
   withCredentials: true, 
 });
 
